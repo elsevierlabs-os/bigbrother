bc2ba39d320f2d1996d21bcad87bcd37333dccb0
Marco Stagni <m.stagni@elsevier.com>
2020-02-24 16:45:30 +0000

    0.3.9

--- dist/runner/Runner.js
+++ dist/runner/Runner.js
@@ -9,19 +9,11 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-var _toConsumableArray2 = _interopRequireDefault(require("@babel/runtime/helpers/toConsumableArray"));
-
 var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
 
 var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
-
-var _Suite = _interopRequireDefault(require("../tests/Suite"));
-
-var _Browser = _interopRequireDefault(require("../browser/Browser"));
-
-var _functions = require("../lib/functions");
 
 var _httpClient = require("../lib/httpClient");
 
@@ -31,103 +23,30 @@ var _process = require("../lib/utils/process");
 
 var _ProcessRunner = _interopRequireWildcard(require("./ProcessRunner"));
 
+var _TestRunner = _interopRequireDefault(require("./TestRunner"));
+
 var _FileReader = _interopRequireDefault(require("../lib/FileReader"));
 
 var _config = require("../config");
+
+var _constants = require("../lib/constants");
 
 var Runner =
 /*#__PURE__*/
 function () {
   function Runner() {
-    var _this = this;
-
     (0, _classCallCheck2["default"])(this, Runner);
-    (0, _defineProperty2["default"])(this, "executeTestSuites", function () {
-      var tests = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
-      var suites = tests.map(function (_ref) {
-        var filename = _ref.filename,
-            content = _ref.content;
-        return new _Suite["default"](filename, content, _this.browser);
-      });
-      return (0, _functions.PromiseSerial)(suites.map(function (s) {
-        return function () {
-          return s.execute();
-        };
-      })).then(_this.evaluateResults);
-    });
-    (0, _defineProperty2["default"])(this, "evaluateResults", function (suites) {
-      var failed = suites.reduce(function (total, suite) {
-        return [].concat((0, _toConsumableArray2["default"])(total), (0, _toConsumableArray2["default"])(suite.filter(function (test) {
-          return !test.success;
-        })));
-      }, []);
-      var failedCount = failed.length;
-      var suitesCount = suites.length;
-      (0, _printer.printDelimiter)();
-
-      if (failedCount > 0) {
-        (0, _printer.printRunnerFailure)(suitesCount, failedCount);
-
-        _this.printFailures(failed);
-      } else {
-        (0, _printer.printRunnerSuccess)(suitesCount);
-      }
-
-      _this.failed = failed;
-    });
-    (0, _defineProperty2["default"])(this, "printFailures", function (failed) {
-      (0, _printer.printNewLines)();
-      failed.forEach(function (test) {
-        (0, _printer.printTitleTest)(test.title);
-        (0, _printer.printFailedTest)(test.reason.message);
-        (0, _printer.printNewLines)(1);
-      });
-    });
-    (0, _defineProperty2["default"])(this, "cleanup", function () {
-      (0, _printer.printInfo)('Performing Runner cleanup.');
-
-      _ProcessRunner["default"].executePostCommand();
-
-      _ProcessRunner["default"].stop(_ProcessRunner.BEFORE)["catch"](_printer.printException)["finally"](_this.terminate);
-    });
-    (0, _defineProperty2["default"])(this, "terminate", function () {
-      (0, _printer.printInfo)('Terminating Runner.');
-      (0, _process.exitProcess)(_this.failed.length);
-    });
-    (0, _defineProperty2["default"])(this, "startBrowser", function () {
-      (0, _printer.printInfo)('Starting Browser.');
-      _this.browser = new _Browser["default"]((0, _config.getConfig)());
-      return _this.browser.launch();
-    });
-    (0, _defineProperty2["default"])(this, "stop", function () {
-      if (_this.browser) {
-        return _this.browser.close();
-      }
-
-      return Promise.resolve();
-    });
-    this.browser = null;
-    this.suites = [];
-    this.failed = [];
   }
 
-  (0, _createClass2["default"])(Runner, [{
+  (0, _createClass2["default"])(Runner, null, [{
     key: "setup",
     value: function setup(configuration) {
       (0, _config.storeConfiguration)(configuration);
-      (0, _process.onUserInterrupt)(this.stop);
+      (0, _process.onUserInterrupt)(Runner.stop);
 
       _ProcessRunner["default"].executePreCommand();
     }
   }, {
-    key: "start",
-    value: function start(config) {
-      (0, _printer.printInfo)('Starting Runner.');
-      this.setup(config);
-      (0, _printer.printBigBrother)();
-      Runner.checkTargetApplicationIsRunning().then(this.startBrowser).then(_FileReader["default"].readFiles).then(this.executeTestSuites).then(this.stop)["catch"](_printer.printException)["finally"](this.cleanup);
-    }
-  }], [{
     key: "checkTargetApplicationIsRunning",
     value: function checkTargetApplicationIsRunning() {
       var _getConfig = (0, _config.getConfig)(),
@@ -137,9 +56,31 @@ function () {
 
       return (0, _httpClient.pingEndpoint)(baseUrl, maxRetries, retryTimeout);
     }
+  }, {
+    key: "start",
+    value: function start(config) {
+      (0, _printer.printInfo)(_constants.RUNNER_STARTING_MESSAGE);
+      Runner.setup(config);
+      (0, _printer.printBigBrother)();
+      Runner.checkTargetApplicationIsRunning().then(_TestRunner["default"].startBrowser).then(_FileReader["default"].readFiles).then(_TestRunner["default"].executeTestSuites).then(Runner.stop)["catch"](_printer.printException)["finally"](Runner.cleanup);
+    }
   }]);
   return Runner;
 }();
 
+(0, _defineProperty2["default"])(Runner, "cleanup", function () {
+  (0, _printer.printInfo)(_constants.RUNNER_CLEANUP_MESSAGE);
+
+  _ProcessRunner["default"].executePostCommand();
+
+  _ProcessRunner["default"].stop(_ProcessRunner.BEFORE)["catch"](_printer.printException)["finally"](Runner.terminate);
+});
+(0, _defineProperty2["default"])(Runner, "terminate", function () {
+  (0, _printer.printInfo)(_constants.RUNNER_TERMINATION_MESSAGE);
+  (0, _process.exitProcess)(_TestRunner["default"].getFailures().length);
+});
+(0, _defineProperty2["default"])(Runner, "stop", function () {
+  return _TestRunner["default"].stopBrowser();
+});
 var _default = Runner;
 exports["default"] = _default;
---
+++ dist/runner/TestRunner.js
@@ -0,0 +1,122 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _toConsumableArray2 = _interopRequireDefault(require("@babel/runtime/helpers/toConsumableArray"));
+
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
+
+var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
+
+var _ = require("..");
+
+var _Browser = _interopRequireDefault(require("../browser/Browser"));
+
+var _config = require("../config");
+
+var _Suite = _interopRequireDefault(require("../tests/Suite"));
+
+var _functions = require("../lib/functions");
+
+var _printer = require("../lib/printer");
+
+var _constants = require("../lib/constants");
+
+var TestRunner =
+/*#__PURE__*/
+function () {
+  function TestRunner() {
+    var _this = this;
+
+    (0, _classCallCheck2["default"])(this, TestRunner);
+    (0, _defineProperty2["default"])(this, "startBrowser", function () {
+      (0, _.printInfo)(_constants.BROWSER_STARTING_MESSAGE);
+      _this.browser = new _Browser["default"]((0, _config.getConfig)());
+      return _this.browser.launch();
+    });
+    (0, _defineProperty2["default"])(this, "stopBrowser", function () {
+      if (_this.hasBrowser()) {
+        return _this.browser.close();
+      }
+
+      return Promise.reject(_constants.BROWSER_NOT_INITIALISED);
+    });
+    (0, _defineProperty2["default"])(this, "mapTestToNewSuite", function (_ref) {
+      var filename = _ref.filename,
+          content = _ref.content;
+      return new _Suite["default"](filename, content, _this.browser);
+    });
+    (0, _defineProperty2["default"])(this, "mapSuitesToExecution", function (suite) {
+      return function () {
+        return suite.execute();
+      };
+    });
+    (0, _defineProperty2["default"])(this, "mapTestsToPromises", function (tests) {
+      return tests.map(_this.mapTestToNewSuite).map(_this.mapSuitesToExecution);
+    });
+    (0, _defineProperty2["default"])(this, "executeTestSuites", function () {
+      var tests = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+      return (0, _functions.PromiseSerial)(_this.mapTestsToPromises(tests)).then(_this.evaluateResults);
+    });
+    (0, _defineProperty2["default"])(this, "evaluateResults", function (suites) {
+      _this.failures = TestRunner.extractFailure(suites);
+      var failuresCount = _this.failures.length;
+      var suitesCount = suites.length;
+      (0, _printer.printDelimiter)();
+
+      if (failuresCount > 0) {
+        (0, _printer.printRunnerFailure)(suitesCount, failuresCount);
+
+        _this.printFailures();
+      } else {
+        (0, _printer.printRunnerSuccess)(suitesCount);
+      }
+    });
+    (0, _defineProperty2["default"])(this, "printFailures", function () {
+      (0, _.printNewLines)();
+
+      _this.getFailures().forEach(function (test) {
+        (0, _printer.printTitleTest)(test.title);
+        (0, _printer.printFailedTest)(test.reason.message);
+        (0, _.printNewLines)(1);
+      });
+    });
+    this.failures = [];
+    this.suites = [];
+    this.browser = null;
+  }
+
+  (0, _createClass2["default"])(TestRunner, [{
+    key: "hasBrowser",
+    value: function hasBrowser() {
+      return !!this.browser;
+    }
+  }, {
+    key: "getFailures",
+    value: function getFailures() {
+      return this.failures;
+    }
+  }], [{
+    key: "extractFailure",
+    value: function extractFailure() {
+      var suites = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+      return suites.reduce(function (total, suite) {
+        return [].concat((0, _toConsumableArray2["default"])(total), (0, _toConsumableArray2["default"])(suite.filter(function (test) {
+          return !test.success;
+        })));
+      }, []);
+    }
+  }]);
+  return TestRunner;
+}();
+
+var _default = new TestRunner();
+
+exports["default"] = _default;
